d5a3a2944d19d00f15d5ae7d8c5ecaec98c81363
John Jack Franji <jeanjack.franji@gmail.com>
2016-07-18 22:03:23 +0200

    Bug Fixes

--- MobileL2P/App_Start/BundleConfig.cs
+++ MobileL2P/App_Start/BundleConfig.cs
@@ -8,15 +8,15 @@ namespace MobileL2P
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                       "~/Scripts/jquery-{version}.js"));
+            //bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+            //           "~/Scripts/jquery-{version}.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
+            //bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+            //            "~/Scripts/jquery.validate*"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/lolliclock.css",
-                      "~/Content/site.css"));
+            //bundles.Add(new StyleBundle("~/Content/css").Include(
+            //          "~/Content/lolliclock.css",
+            //          "~/Content/site.css"));
         }
     }
 }
